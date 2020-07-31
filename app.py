cda6f60aae73ccfbbb01971bb334e095fc8eab44
Colin Chen <colinc@fastmail.com>
2020-07-31 15:58:57 -0700

    fix app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-import create_groups.parser as parser
+import logging
+
+from submodule_utils.logging import logger_factory
+from create_groups.parser import create_parser
 from create_groups import *
 
+logger_factory()
+logger = logging.getLogger('create_groups')
+
 if __name__ == "__main__":
+    parser = create_parser()
     config = parser.get_args()
     gc = GroupCreator(config)
     gc.run()
